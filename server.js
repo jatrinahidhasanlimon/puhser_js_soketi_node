d5edbae75147061b09743ba90d0ab3d1363edba0
NahidHasanLimon <nh.limon@yahoo.com>
2022-12-26 01:54:18 +0600

    soketi .env in production checked

--- server.js
+++ server.js
@@ -21,6 +21,16 @@ const pusherPrduction = new Pusher({
     port: 8085, // optional, defaults to 80 for non-TLS connections and 443 for TLS connections
     // encryptionMasterKeyBase64: ENCRYPTION_MASTER_KEY, // a base64 string which encodes 32 bytes, used to derive the per-channel encryption keys (see below!)
   })
+  const pusherPrductionTwo = new Pusher({
+    appId: "app_1234",
+    key: "app_1234",
+    secret: "app_1234",
+    useTLS: false, // optional, defaults to false
+    cluster: "CLUSTER", // if `host` is present, it will override the `cluster` option.
+    host: "18.138.218.206", // optional, defaults to api.pusherapp.com
+    port: 8085, // optional, defaults to 80 for non-TLS connections and 443 for TLS connections
+    // encryptionMasterKeyBase64: ENCRYPTION_MASTER_KEY, // a base64 string which encodes 32 bytes, used to derive the per-channel encryption keys (see below!)
+  })
 const pusher = new Pusher({ 
     appId: "app_1234",
     key: "app_1234",
@@ -47,9 +57,10 @@ const pusher2 = new Pusher({
 
   app.use('/trigger-notification',( req, res) => {
 
-    pusher.trigger("my-channel", "my-event", { message: "hello world" });    
-    pusher2.trigger("my-channel", "my-event", { message: "hello world from pusher two"});    
+    // pusher.trigger("my-channel", "my-event", { message: "hello world" });    
+    // pusher2.trigger("my-channel", "my-event", { message: "hello world from pusher two"});    
     pusherPrduction.trigger("my-channel-dev", "my-event-dev", { message: "hello dev env"});    
+    pusherPrductionTwo.trigger("my-channel-dev", "my-event-dev", { message: "hello dev env"});    
 
 
     res.send('Notification send.')
